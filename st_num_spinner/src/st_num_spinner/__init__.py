67406fb253f98cd6e3173677dd8963bd693b980f
281developer <dave.cowden@gmail.com>
2024-03-17 14:47:26 -0400

    cool control

---
+++ st_num_spinner/src/st_num_spinner/__init__.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Optional
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Tell streamlit that there is a component called st_num_spinner,
+# and that the code to display that component is in the "frontend" folder
+frontend_dir = (Path(__file__).parent / "frontend").absolute()
+
+
+_component_func = components.declare_component(
+    "frc_scoring_tracker",
+	path=str(frontend_dir)
+)
+
+# Create the python function that will be called
+def frc_scoring_tracker(
+    key: Optional[str] = None,
+):
+    """
+    Add a descriptive docstring
+    """
+    component_value = _component_func(
+        key=key
+    )
+    DEFAULT_VALUES=[0,0,0,0,0,0,0,0];
+    if component_value is None:
+        return DEFAULT_VALUES
+    else:
+        return component_value
+    return component_value
+
+
+def main():
+    st.write("## Scoring Summary")
+    value = frc_scoring_tracker(key="auto_scoring")
+
+    st.write(" ## Value:")
+    st.write(value)
+
+
+if __name__ == "__main__":
+    main()
---
+++ st_num_spinner/src/st_num_spinner/frontend/main.js
@@ -0,0 +1,105 @@
+// The `Streamlit` object exists because our html file includes
+// `streamlit-component-lib.js`.
+// If you get an error about "Streamlit" not being defined, that
+// means you're missing that file.
+
+var currentValues = [
+    0,0,
+    0,0,
+    0,0,
+    0,0
+]
+
+
+function update() {
+  Streamlit.setComponentValue(currentValues)
+}
+
+/**
+ * The component's render function. This will be called immediately after
+ * the component is initially loaded, and then again every time the
+ * component gets new data from Python.
+ */
+function onRender(event) {
+  // Only run the render code the first time the component is loaded.
+  if (!window.rendered) {
+
+    // later, convert to more dynamic
+    //const {labels} = event.detail.args;
+
+    const amp_score = document.getElementById("amp_score");
+    const amp_miss = document.getElementById("amp_miss");
+    const amp_value = document.getElementById("amp_value");
+
+    const subwoofer_score = document.getElementById("subwoofer_score");
+    const subwoofer_miss = document.getElementById("subwoofer_miss");
+    const subwoofer_value = document.getElementById("subwoofer_value");
+
+    const podium_score = document.getElementById("podium_score");
+    const podium_miss = document.getElementById("podium_miss");
+    const podium_value = document.getElementById("podium_value");
+
+    const far_score = document.getElementById("far_score");
+    const far_miss = document.getElementById("far_miss");
+    const far_value = document.getElementById("far_value");
+
+    amp_score.onclick = function(e){
+        currentValues[0]++;
+        currentValues[1]++;
+        update();
+        amp_value.innerText = currentValues[0] + "/" + currentValues[1];
+    }
+    amp_miss.onclick = function(e){
+        currentValues[1]++;
+        update();
+        amp_value.innerText = currentValues[0] + "/" + currentValues[1];
+    }
+
+    subwoofer_score.onclick = function(e){
+        currentValues[2]++;
+        currentValues[3]++;
+        update();
+        subwoofer_value.innerText = currentValues[2] + "/" + currentValues[3];
+    }
+    subwoofer_miss.onclick = function(e){
+        currentValues[3]++;
+        update();
+        subwoofer_value.innerText = currentValues[2] + "/" + currentValues[3];
+    }
+
+    podium_score.onclick = function(e){
+        currentValues[4]++;
+        currentValues[5]++;
+        update();
+        podium_value.innerText = currentValues[4] + "/" + currentValues[5];
+    }
+    podium_miss.onclick = function(e){
+        currentValues[5]++;
+        update();
+        podium_value.innerText = currentValues[4] + "/" + currentValues[5];
+    }
+
+    far_score.onclick = function(e){
+        currentValues[6]++;
+        currentValues[7]++;
+        update();
+        far_value.innerText = currentValues[6] + "/" + currentValues[7];
+    }
+    far_miss.onclick = function(e){
+        currentValues[7]++;
+        update();
+        far_value.innerText = currentValues[6] + "/" + currentValues[7];
+    }
+    update();
+
+    window.rendered = true
+  }
+}
+
+// Render the component whenever python send a "render event"
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
+// Tell Streamlit that the component is ready to receive events
+Streamlit.setComponentReady()
+// Render with the correct height, if this is a fixed-height component
+//Streamlit.setFrameHeight(100)
+
