dfe6fdef11ed9e4f2482f26a24ce504523b8b7ce
FortressBen <fortressben@gmail.com>
2024-03-29 15:09:01 -0400

    fixed accuracy

--- team_analysis.py
+++ team_analysis.py
@@ -148,9 +148,9 @@ def team_summary(analzyed_data):
         amp_completed_auto=('amp.completed.auto', 'sum'),
         speaker_subwoofer_completed_auto=('speaker.subwoofer.completed.auto','sum'),
         speaker_subwoofer_attempted_auto=('speaker.subwoofer.attempted.auto','sum'),
-        speaker_podium_completed_auto=('speaker.subwoofer.completed.auto','sum'),
+        speaker_podium_completed_auto=('speaker.podium.completed.auto','sum'),
         speaker_podium_attempted_auto=('speaker.podium.attempted.auto','sum'),
-        speaker_medium_completed_auto=('speaker.subwoofer.completed.auto','sum'),
+        speaker_medium_completed_auto=('speaker.medium.completed.auto','sum'),
         speaker_medium_attempted_auto=('speaker.medium.attempted.auto','sum'),
 
         amp_attempted_teleop=('amp.attempted.teleop', 'sum'),
@@ -159,7 +159,7 @@ def team_summary(analzyed_data):
         speaker_subwoofer_attempted_teleop=('speaker.subwoofer.attempted.teleop', 'sum'),
         speaker_podium_completed_teleop=('speaker.podium.completed.teleop', 'sum'),
         speaker_podium_attempted_teleop=('speaker.podium.attempted.teleop', 'sum'),
-        speaker_medium_completed_teleop=('speaker.subwoofer.completed.teleop', 'sum'),
+        speaker_medium_completed_teleop=('speaker.medium.completed.teleop', 'sum'),
         speaker_medium_attempted_teleop=('speaker.medium.attempted.teleop', 'sum'),
 
         
