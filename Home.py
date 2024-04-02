44186788d8d9d5952f7f38e4f3e38ddfb7f5e509
281developer <dave.cowden@gmail.com>
2024-04-02 18:53:04 -0400

    add logo

--- Home.py
+++ Home.py
@@ -5,8 +5,10 @@ config.configure(st.secrets)
 st.set_page_config(layout="wide")
 st.title("281 2024 DCMP Scouting")
 
+st.image("./281_logo.png", width=400)
 
 
+st.subheader("(Open the sidebar to access the app features!)")
 
 
 
